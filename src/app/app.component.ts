acf9a654862cc366cd1f494211942b85d16f03df
Yuriy Cherkasov <yuriy@code-care.pro>
2019-10-03 16:54:47 +0300

    Detect when user is online and logged in using rxjs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { map, delay, switchMap, takeWhile, startWith, merge, retryWhen, tap } from 'rxjs/operators';
+import { Observable, fromEvent, interval, combineLatest, Subscription } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Observable, fromEvent, from, interval, of } from 'rxjs';
-import { map, switchMap, mergeMap, takeWhile } from 'rxjs/operators';
 
-
+const githubUsers = `https://api.github.com/users?per_page=2`;
 
 @Component({
   selector: 'app-root',
@@ -11,47 +11,81 @@ import { map, switchMap, mergeMap, takeWhile } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  private apiEndpoint = 'http://localhost:4200';
-  private networkStatus: string;
+  private isOnlineSubscr: Subscription;
+  private isLoginSubscr: Subscription;
+  private longPollSubscr: Subscription;
+  isUserOnline: boolean;
+  isUserLoggedIn: boolean;
+  isUserOnline$: Observable<boolean>;
+  isUserLoggedIn$: Observable<boolean>;
+  @ViewChild('loginButton', { static: false }) button: ElementRef;
 
   constructor(
     private http: HttpClient,
-  ) {
-    this.networkStatus = window.navigator.onLine ? 'online' : 'offline';
-    console.log('networkStatus', this.networkStatus);
-  }
+  ) { }
 
   ngOnInit() {
-    this.getOnlineStatus()
-      .subscribe((status: string) => {
-        this.networkStatus = status;
-        console.log('networkStatus', this.networkStatus);
-        if (status === 'online') {
-          this.longPollingRequests();
-        }
-      })
-    this.longPollingRequests();
+    this.isUserOnline$ = this.getOnlineStatus();
+    this.isOnlineSubscr = this.isUserOnline$.subscribe((isOnline: boolean) => {
+      this.isUserOnline = isOnline;
+      console.log({ isOnline });
+      if (isOnline) {
+        this.longPollingRequests();
+      }
+    });
   }
 
-  getOnlineStatus(): Observable<string> {
-    const events = ['online', 'offline']
-    return from(events)
-      .pipe(
-        mergeMap(event => fromEvent(window, event)),
-        map((event: Event) => event.type)
+  ngAfterViewInit() {
+    const isLoginEvents$ = fromEvent(this.button.nativeElement, 'click').pipe(
+      map(() => this.isUserLoggedIn),
+    );
+    this.isLoginSubscr = isLoginEvents$.subscribe(res => {
+      if (this.isUserOnline) {
+        this.isUserLoggedIn = !res;
+      }
+    });
+
+    this.isUserLoggedIn$ = combineLatest(
+      isLoginEvents$,
+      this.isUserOnline$,
+    ).pipe(
+      map((values: Array<boolean>) => values.every(i => i))
+    );
+  }
+
+  private getOnlineStatus(): Observable<boolean> {
+    const WINDOW = window;
+    const onlineEvents$ = fromEvent(WINDOW, 'online').pipe(
+      map(() => true)
+    );
+    const offlineEvents$ = fromEvent(WINDOW, 'offline').pipe(
+      map(() => false)
+    );
+    const isUserOnline$ = onlineEvents$.pipe(
+      merge(offlineEvents$),
+      startWith(WINDOW.navigator.onLine)
+    );
+    return isUserOnline$;
+  }
+
+  private longPollingRequests() {
+    this.longPollSubscr = interval(2000).pipe(
+      switchMap(() => this.http.get(githubUsers)),
+      takeWhile(() => this.isUserOnline),
+      retryWhen(errors =>
+        errors.pipe(
+          tap(error => console.log('Error: ', error)),
+          delay(3000)
+        )
       )
+    ).subscribe(data => { console.log('Data', data); });
   }
 
-  longPollingRequests() {
-    interval(1500).pipe(
-      switchMap(() => of(1)),
-      // switchMap(() => this.http.get(this.apiEndpoint)),
-      takeWhile(() => this.networkStatus === 'online')
-    ).subscribe(
-      data => { console.log('Data', data) },
-      error => { console.log('Error: ', error) }
-    )
+  ngOnDestroy() {
+    this.isOnlineSubscr.unsubscribe();
+    this.isLoginSubscr.unsubscribe();
+    this.longPollSubscr.unsubscribe();
   }
 }
